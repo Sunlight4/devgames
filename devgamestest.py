c325be7caa6f5419088f3d6cbf1a90f1e9fd9c45
Sunlight4 <ethan.saff@gmail.com>
2014-11-10 19:51:31 -0500

    New Colors + Updated Test

--- devgamestest.py
+++ devgamestest.py
@@ -1,9 +1,12 @@
 import pygame
+import devgames
 from devgames import main, util
 from devgames.vector import Vector
+devgames.newvital("You Lose!")
 class KeyboardSquareBlock(util.KeyboardControlledObject, util.SquareBlock):pass
 block1=KeyboardSquareBlock((255,0,0), 32, 320, 100)
 block2=util.SquareBlock((0,0,255), 32, 200, 50)
 block1.killoncontact(block2)
+devgames.vital[0][0].add(block1)
 main({}, {}, block1, block2)
 pygame.quit()
